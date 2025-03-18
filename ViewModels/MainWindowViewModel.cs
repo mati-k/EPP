cf3e18e66e3de688bc08e9f93df8db1bf2d34178
mati <matik.github@gmail.com>
2025-03-18 18:09:15 +0100

    feat: add initial starting page

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace EPP.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(StartCommand))]
+        private string _eventFile = string.Empty;
+
+        [ObservableProperty]
+        private string _localizationFile = string.Empty;
+
+        [ObservableProperty]
+        private bool _useBackups = true;
+
+        [ObservableProperty]
+        private ObservableCollection<string> _sourceDirectories = new();
+
+        [RelayCommand(CanExecute = nameof(CanStart))]
+        public void Start()
+        {
+        }
+
+        public bool CanStart()
+        {
+            return !string.IsNullOrEmpty(EventFile) && SourceDirectories.Count > 0;
+        }
+    }
+}
